97cfa5a590a31ccbbaed48142b847678424675c2
Ting Chun Liu <8765498@gmail.com>
2022-04-06 15:34:40 +0200

    fix syntax

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 from OSC import OSCServer,OSCClient, OSCMessage
-server = OSCServer((“0.0.0.0”,9527))
+server = OSCServer(("0.0.0.0",9527))
 def msg_callback(path, tags, args, source):
-       print(int(args[0]))
-server.addMsgHandler( “/msg”,msg_callback)
+    print(int(args[0]))
+server.addMsgHandler("/msg",msg_callback)
 while True:
-       server.handle_request()
+    server.handle_request()
