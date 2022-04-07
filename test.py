d711a876a4b0e01e2ece20136cedb792c9b97698
Ting Chun Liu <8765498@gmail.com>
2022-04-07 19:35:00 +0200

    add time

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import multiprocessing
 import os
 import signal
 import sys
+import time
 proc1 = subprocess.Popen(args=['omxplayer','-o','both','val1.mp3'])
 time.sleep(20)
 subprocess.call(['pkill','-P',str(proc1.pid)])
